f86b7fca6b89a255c4604597f3425b58d86ab877
DanielBLabriola <daniellabriola@gmail.com>
2016-11-18 22:13:24 -0200

    projeto versão final 1.0

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -27,13 +27,20 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.web.WebEngine;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-
-
+import java.util.Scanner;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -73,6 +80,7 @@ public class Controller implements Initializable  {
 	
 	int NumerodeMeses;
 	double parcela;
+	boolean dados=false;
 	
 	
 	public TabPane TabPane;
@@ -346,6 +354,13 @@ public class Controller implements Initializable  {
 	    	System.out.println("Preço 120:: "+PLED120);
 	    	System.out.println("Preço 60::"+PLED60);
 	    	}
+	    @FXML
+	    private void btnJacad(){
+	    	dados=true;
+	    	TabNumS.setDisable(true);
+	    	TabPane.getSelectionModel().select(TabTabela);
+	    	TabTabela.setDisable(false);
+	    }
 	    
 	  //passo 4 //loop
 	    @FXML
@@ -439,6 +454,7 @@ public class Controller implements Initializable  {
 	    @FXML
 	    private void inciPasso5(){
 	    	
+			if(dados==false){
 	    	//tabela
 	    	colSala.setCellValueFactory(new PropertyValueFactory<>("Sala"));
 	        colConsumo.setCellValueFactory(new PropertyValueFactory<>("Consumo"));
@@ -454,44 +470,154 @@ public class Controller implements Initializable  {
 	        		 observableListTabela = FXCollections.observableArrayList(lisTabela);
 
 	        	        Tabela.setItems(observableListTabela);
-	        	        
-	        //total	        
-	      edificio.setCellValueFactory(new PropertyValueFactory<>("Edificio"));
-	       tconsumo.setCellValueFactory(new PropertyValueFactory<>("Consumo"));
-	       tcusto.setCellValueFactory(new PropertyValueFactory<>("Custo"));
-	       tcustokwh.setCellValueFactory(new PropertyValueFactory<>("tcustokwh"));
-	       
-	       CustoKwhLedT.setCellValueFactory(new PropertyValueFactory<>("custokwhled"));
-	       ConsumoLedT.setCellValueFactory(new PropertyValueFactory<>("consumoled"));
-	       
+	        	        //total	        
+	        		      edificio.setCellValueFactory(new PropertyValueFactory<>("Edificio"));
+	        		       tconsumo.setCellValueFactory(new PropertyValueFactory<>("Consumo"));
+	        		       tcusto.setCellValueFactory(new PropertyValueFactory<>("Custo"));
+	        		       tcustokwh.setCellValueFactory(new PropertyValueFactory<>("tcustokwh"));
+	        		       
+	        		       CustoKwhLedT.setCellValueFactory(new PropertyValueFactory<>("custokwhled"));
+	        		       ConsumoLedT.setCellValueFactory(new PropertyValueFactory<>("consumoled"));
+	        		       
+	        		        
+	        		       double[] custoT=new double[NumSalas];
+	        		       double[] consumoT=new double[NumSalas];
+	        		       double[] custokwhT=new double[NumSalas];
+	        		       double[] consumoledT=new double[NumSalas];
+	        		       double[] custokwhledT=new double[NumSalas];
+	        		       
+	        		       for(int i=0;i<NumSalas;i++){
+	        		    	   System.out.println("teste");
+	        		    	   custoT[i]=Double.parseDouble(custodasLampadas.get(i));
+	        		    	   consumoT[i]=Double.parseDouble(consumo.get(i));
+	        		    	   custokwhT[i]=Double.parseDouble(custoKWh.get(i));
+	        		    	   consumoledT[i]=Double.parseDouble(consumoLED.get(i));
+	        		    	   custokwhledT[i]=Double.parseDouble(custoKWhLED.get(i));
+	        		    	   
+	        		    	   
+	        		    	   totalLampadas=totalLampadas+custoT[i];
+	        		    	   totalConsumo=totalConsumo+consumoT[i];
+	        		    	   custokWhtotal=custokWhtotal+custokwhT[i];
+	        		    	   totalConsumoLED=totalConsumoLED+consumoledT[i];
+	        		    	   custokWhtotalLED=custokWhtotalLED+custokwhledT[i];
+	        		    	   
+	        		       }
+	        		       
+	        		       Total T=new Total("Total",String.valueOf(totalConsumo),String.valueOf(custokWhtotal),
+	        		    		   String.valueOf(totalLampadas),String.valueOf(totalConsumoLED),String.valueOf(custokWhtotalLED));
+	        		       lisTotal.add(T);
+	        		       observableListTotal=FXCollections.observableArrayList(lisTotal);
+	        		       total.setItems(observableListTotal);
+	    	}
+	    	if(dados==true){
+	    		
+	    		//limpar dados nos Arrylist
+	    		consumo.clear();
+	    		custoKWh.clear();
+	    		custodasLampadas.clear();
+	    		consumoLED.clear();
+	    		custoKWhLED.clear();
+	    		
+	    		
+	    		colSala.setCellValueFactory(new PropertyValueFactory<>("Sala"));
+		        colConsumo.setCellValueFactory(new PropertyValueFactory<>("Consumo"));
+		        colCusto.setCellValueFactory(new PropertyValueFactory<>("Custo"));
+		        CustoKWh.setCellValueFactory(new PropertyValueFactory<>("custoKWh"));
+		        ConsumoLed.setCellValueFactory(new PropertyValueFactory<>("ConsumoLed"));
+		        CustoKwhLed.setCellValueFactory(new PropertyValueFactory<>("CustoKwhLed"));
+		       		        				        		
+	    		 File arquivoCSV = new File("tabela.csv");
+	              
+	              try{
+	                  
+	                  //cria um scanner para ler o arquivo
+	                  Scanner leitor = new Scanner(arquivoCSV);
+	                  
+	                  //variavel que armazenara as linhas do arquivo
+	                  String linhasDoArquivo = new String();
+	                  
+	                  //ignora a primeira linha do arquivo
+	                  leitor.nextLine();
+	                  
+	                  //percorre todo o arquivo
+	                  while(leitor.hasNext()){
+	                      
+	                      //recebe cada linha do arquivo
+	                      linhasDoArquivo = leitor.nextLine();
+	                      
+	                      //separa os campos entre as virgulas de cada linha
+	                      String[] valoresEntreVirgulas = linhasDoArquivo.split(";");
+	                      
+	                      //imprime a coluna que quiser
+	                      CTabela tabela= new CTabela(Integer.parseInt(valoresEntreVirgulas[0]),valoresEntreVirgulas[1],
+	                    		  valoresEntreVirgulas[2],valoresEntreVirgulas[3],valoresEntreVirgulas[4],valoresEntreVirgulas[5]);
+	                      lisTabela.add(tabela);
+	                      consumo.add(String.valueOf(valoresEntreVirgulas[1]));
+	                      custoKWh.add(String.valueOf(valoresEntreVirgulas[2]));
+	                      custodasLampadas.add(String.valueOf(valoresEntreVirgulas[3]));
+	                      consumoLED.add(String.valueOf(valoresEntreVirgulas[4]));
+	                      custoKWhLED.add(String.valueOf(valoresEntreVirgulas[5]));
+	                  }
+	                  observableListTabela = FXCollections.observableArrayList(lisTabela);
+
+	        	        Tabela.setItems(observableListTabela);
+	        	      //total	        
+	        		      edificio.setCellValueFactory(new PropertyValueFactory<>("Edificio"));
+	        		       tconsumo.setCellValueFactory(new PropertyValueFactory<>("Consumo"));
+	        		       tcusto.setCellValueFactory(new PropertyValueFactory<>("Custo"));
+	        		       tcustokwh.setCellValueFactory(new PropertyValueFactory<>("tcustokwh"));
+	        		       
+	        		       CustoKwhLedT.setCellValueFactory(new PropertyValueFactory<>("custokwhled"));
+	        		       ConsumoLedT.setCellValueFactory(new PropertyValueFactory<>("consumoled"));
+	        		       
+	        		        
+	        		       double[] custoT=new double[consumo.size()];
+	        		       double[] consumoT=new double[consumo.size()];
+	        		       double[] custokwhT=new double[consumo.size()];
+	        		       double[] consumoledT=new double[consumo.size()];
+	        		       double[] custokwhledT=new double[consumo.size()];
+	        		       
+	        		       for(int i=0;i<consumo.size();i++){
+	        		    	   System.out.println("teste");
+	        		    	   custoT[i]=Double.parseDouble(custodasLampadas.get(i));
+	        		    	   consumoT[i]=Double.parseDouble(consumo.get(i));
+	        		    	   custokwhT[i]=Double.parseDouble(custoKWh.get(i));
+	        		    	   consumoledT[i]=Double.parseDouble(consumoLED.get(i));
+	        		    	   custokwhledT[i]=Double.parseDouble(custoKWhLED.get(i));
+	        		    	   
+	        		    	   
+	        		    	   totalLampadas=totalLampadas+custoT[i];
+	        		    	   totalConsumo=totalConsumo+consumoT[i];
+	        		    	   custokWhtotal=custokWhtotal+custokwhT[i];
+	        		    	   totalConsumoLED=totalConsumoLED+consumoledT[i];
+	        		    	   custokWhtotalLED=custokWhtotalLED+custokwhledT[i];
+	        		    	   
+	        		       }
+	        		       
+	        		       Total T=new Total("Total",String.valueOf(totalConsumo),String.valueOf(custokWhtotal),
+	        		    		   String.valueOf(totalLampadas),String.valueOf(totalConsumoLED),String.valueOf(custokWhtotalLED));
+	        		       lisTotal.add(T);
+	        		       observableListTotal=FXCollections.observableArrayList(lisTotal);
+	        		       total.setItems(observableListTotal);
+	              }catch(FileNotFoundException e){
+	            	  Alert alert = new Alert(Alert.AlertType.INFORMATION);
+	     		     
+	 		         alert.setContentText("Não foram encontradas Salas cadastradas!");
+	 		         alert.show();
+	 		        consumo.clear();
+		    		custoKWh.clear();
+		    		custodasLampadas.clear();
+		    		consumoLED.clear();
+		    		custoKWhLED.clear();
+	 		        TabTabela.setDisable(true);
+	 		    	TabPane.getSelectionModel().select(TabNumS);
+	 		    	TabNumS.setDisable(false);
+	 		         
+	 		         
+	 		         
+	              }
+	    	}
 	        
-	       double[] custoT=new double[NumSalas];
-	       double[] consumoT=new double[NumSalas];
-	       double[] custokwhT=new double[NumSalas];
-	       double[] consumoledT=new double[NumSalas];
-	       double[] custokwhledT=new double[NumSalas];
-	       
-	       for(int i=0;i<NumSalas;i++){
-	    	   custoT[i]=Double.parseDouble(custodasLampadas.get(i));
-	    	   consumoT[i]=Double.parseDouble(consumo.get(i));
-	    	   custokwhT[i]=Double.parseDouble(custoKWh.get(i));
-	    	   consumoledT[i]=Double.parseDouble(consumoLED.get(i));
-	    	   custokwhledT[i]=Double.parseDouble(custoKWhLED.get(i));
-	    	   
-	    	   
-	    	   totalLampadas=totalLampadas+custoT[i];
-	    	   totalConsumo=totalConsumo+consumoT[i];
-	    	   custokWhtotal=custokWhtotal+custokwhT[i];
-	    	   totalConsumoLED=totalConsumoLED+consumoledT[i];
-	    	   custokWhtotalLED=custokWhtotalLED+custokwhledT[i];
-	    	   
-	       }
-	       
-	       Total T=new Total("Total",String.valueOf(totalConsumo),String.valueOf(custokWhtotal),
-	    		   String.valueOf(totalLampadas),String.valueOf(totalConsumoLED),String.valueOf(custokWhtotalLED));
-	       lisTotal.add(T);
-	       observableListTotal=FXCollections.observableArrayList(lisTotal);
-	       total.setItems(observableListTotal);
 	        		
 	        
 	    	
@@ -509,10 +635,108 @@ public class Controller implements Initializable  {
 	    @FXML
 	    private void Avantpasso6(){
 
+	    	
+	    	if(rbSim.isSelected()==true){
+	    		createCsvFile();
+	    	}
 	    	TabSalvar.setDisable(true);
 	    	TabPane.getSelectionModel().select(TabSimular);
 	    	TabSimular.setDisable(false);
 	    	}
+	    @FXML
+	    private Button a;
+	    @FXML
+	    private Button b;
+	    //teste CSV GRAVAR
+	    
+	    public void createCsvFile(){ 
+	    	//A estrutura try-catch é usada pois o objeto BufferedWriter exige que as
+	    	//excessões sejam tratadas
+	    	try{
+	    		 File arquivo;
+
+	             arquivo = new File("tabela.csv");
+	             FileOutputStream StrW = new FileOutputStream(arquivo);
+	    	//Criação de um buffer para a escrita em uma stream
+	    	//BufferedWriter StrW = new BufferedWriter(new FileWriter("tabela.csv"));
+
+	    	//Escrita dos dados da tabela
+	    	StrW.write(String.valueOf("Sala;Consumo (kw);Custo Kwh (R$);Custo da troca das lampadas (r$);Consumo com LED (kw);Custo Kwh (R$)\n").getBytes());
+	    	for (int i=0;i<NS;i++){
+    			
+    		  	StrW.write((String.valueOf(i+1)+";"+consumo.get(i)+";"+custoKWh.get(i)+";"+custodasLampadas.get(i)+";"+consumoLED.get(i)+";"+custoKWhLED.get(i)+"\n").getBytes()); 
+	    	}
+
+	    	//Fechamos o buffer
+	    	StrW.close(); 
+	    	 Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		       //  alert.setTitle("Este Ã© o tÃ­tulo do Alerta");
+		        // alert.setHeaderText("Este Ã© o cabeÃ§alho do Alerta");
+		         alert.setContentText("dados Salvos com sucesso!");
+		         alert.show();
+	    	}catch (FileNotFoundException ex)
+	    	{ Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		       //  alert.setTitle("Este Ã© o tÃ­tulo do Alerta");
+	        // alert.setHeaderText("Este Ã© o cabeÃ§alho do Alerta");
+	         alert.setContentText("dados não Salvos com sucesso!");
+	         alert.show();
+	    	ex.printStackTrace(); 
+	    	}catch (IOException e)
+	    	{ Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		       //  alert.setTitle("Este Ã© o tÃ­tulo do Alerta");
+	        // alert.setHeaderText("Este Ã© o cabeÃ§alho do Alerta");
+	         alert.setContentText("dados não Salvos com sucesso!");
+	         alert.show();
+	    	e.printStackTrace(); } 
+	    	}
+
+	    	//Agora vamos criar um método que lê um arquivo CSV e imprima na tela as informações:
+
+	    //teste CSV LER
+	         @FXML	    
+	    	public void readCsvFile(){
+	        	 
+	        	//abre um arquivo e cria um file
+	             File arquivoCSV = new File("tabela.csv");
+	              
+	              try{
+	                  
+	                  //cria um scanner para ler o arquivo
+	                  Scanner leitor = new Scanner(arquivoCSV);
+	                  
+	                  //variavel que armazenara as linhas do arquivo
+	                  String linhasDoArquivo = new String();
+	                  
+	                  //ignora a primeira linha do arquivo
+	                  leitor.nextLine();
+	                  
+	                  //percorre todo o arquivo
+	                  while(leitor.hasNext()){
+	                      
+	                      //recebe cada linha do arquivo
+	                      linhasDoArquivo = leitor.nextLine();
+	                      
+	                      //separa os campos entre as virgulas de cada linha
+	                      String[] valoresEntreVirgulas = linhasDoArquivo.split(";");
+	                      
+	                      //imprime a coluna que quiser
+	                      System.out.print(valoresEntreVirgulas[0]+"\t");
+	                      System.out.print(valoresEntreVirgulas[1]+"\t");
+	                      System.out.print(valoresEntreVirgulas[2]+"\t");
+	                      System.out.print(valoresEntreVirgulas[3]+"\t");
+	                      System.out.print(valoresEntreVirgulas[4]+"\t");
+	                      System.out.print(valoresEntreVirgulas[5]+"\t\n");
+	                  }
+	              
+	              }catch(FileNotFoundException e){
+	                  
+	              }
+	        	 
+
+
+	    	}
+	    
+	    
 	    //passo 7
 	    @FXML
 	    private void Avantpasso7(){// avancar
